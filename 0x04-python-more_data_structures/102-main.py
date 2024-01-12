fe9fc145964a58a888a173253f1175982038651f
stonecode6483 <lateefadekola90@gmail.com>
2024-01-12 11:27:56 -0800

    102-complex_delete.py

--- 0x04-python-more_data_structures/102-main.py
+++ 0x04-python-more_data_structures/102-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+
 complex_delete = __import__('102-complex_delete').complex_delete
 print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
-
 a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
 new_dict = complex_delete(a_dictionary, 'C')
 print_sorted_dictionary(a_dictionary)
